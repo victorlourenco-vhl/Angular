6508024214c93d99191207fbd60eae4cc62478d4
Victor Lourenço <victorhlo12@hotmail.com>
2023-10-04 15:09:55 -0300

    LEARN: Criando um segundo componente de forma automática

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimeiroComponente } from './primeiro-componente/primeiro-componente.component';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrimeiroComponente
+    PrimeiroComponente,
+    SegundoComponenteComponent
   ],
   imports: [
     BrowserModule,
