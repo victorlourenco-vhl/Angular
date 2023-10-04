8c87706a4da45fab93acb0d959e2990aaa1dbf9e
Victor Lourenço <victorhlo12@hotmail.com>
2023-10-04 15:01:56 -0300

    LEARN: Criando o primeiro componente manualmente e apagando o código da página principal

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeiroComponente } from './primeiro-componente/primeiro-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponente
   ],
   imports: [
     BrowserModule,
---
+++ primeiro-projeto/src/app/primeiro-componente/primeiro-componente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'primeiro-componente',
+    template: '<h1>Primeiro componente!</h1>'
+})
+
+export class PrimeiroComponente{
+
+
+}
